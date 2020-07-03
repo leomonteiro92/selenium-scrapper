ef9d31640ffc751e21ab11c5220f100b1d03ab52
Leonardo Monteiro <leomonteiro.outsmart@gmail.com>
2020-07-03 16:00:20 -0300

    Remove scrapy

--- imobiliarias.py
+++ imobiliarias.py
@@ -1,5 +1,5 @@
 # pylint: disable=too-many-function-args
-import scrapy
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -7,118 +7,130 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-DEFAULT_TIMEOUT = 10
+DEFAULT_TIMEOUT = 120
 
+name = 'imobiliariasSpider'
+start_urls = ['https://servico.creci-rj.gov.br/spw/ConsultaCadastral/TelaConsultaPubCompleta.aspx']
 
-class CreciImobiliariasSpider(scrapy.Spider):
-    name = 'imobiliariasSpider'
-    start_urls = ['https://servico.creci-rj.gov.br/spw/ConsultaCadastral/TelaConsultaPubCompleta.aspx']
+profile = webdriver.FirefoxProfile()
+profile.accept_untrusted_certs = True
+profile.assume_untrusted_cert_issuer = False
+profile.set_preference('security.tls.version.max', 2)
+profile.set_preference('security.tls.version.min', 1)
+cap = DesiredCapabilities().FIREFOX
+cap["marionete"] = False
+opts = Options()
+opts.headless = True
+driver = webdriver.Firefox(
+    firefox_profile=profile,
+    capabilities=cap,
+    options=opts
+)
 
-    def __init__(self):
-        profile = webdriver.FirefoxProfile()
-        profile.accept_untrusted_certs = True
-        profile.assume_untrusted_cert_issuer = False
-        profile.set_preference('security.tls.version.max', 2)
-        profile.set_preference('security.tls.version.min', 1)
-        cap = DesiredCapabilities().FIREFOX
-        cap["marionete"] = False
-        opts = Options()
-        opts.headless = True
-        self.driver = webdriver.Firefox(
-            firefox_profile=profile,
-            capabilities=cap,
-            options=opts
+with open('imobiliarias.csv', 'w') as csvfile:
+    w = csv.DictWriter(csvfile, fieldnames=[
+        'numero_registro',
+        'nome',
+        'situacao_cadastral',
+        'endereco',
+        'bairro',
+        'cidade',
+        'cep',
+        'telefone',
+        'responsavel_tecnico'
+    ])
+    w.writeheader()
+
+    driver.get(start_urls[0])
+
+    selectTipoPesquisa = driver.find_element_by_id('cbousuario_B-1')
+    selectTipoPesquisa.click()
+    comboTipoPesquisa = WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+        EC.presence_of_element_located(
+            (By.ID, "cbousuario_DDD_L_LBT"))
+    )
+    selectPJ = comboTipoPesquisa.find_element_by_xpath("//tr[td[contains(text(), 'Pessoa Jurídica')]]")
+    selectPJ.click()
+
+    WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+        EC.invisibility_of_element((By.ID, "ContentPlaceHolder1_Callbackconsulta_LPV"))
+    )
+
+    selectTipoBusca = WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+        EC.element_to_be_clickable((By.ID, "ContentPlaceHolder1_Callbackconsulta_cboTipoBusca2_I"))
+    )
+    selectTipoBusca.click()
+    comboTipoBusca = WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+        EC.presence_of_element_located(
+            (By.ID, "ContentPlaceHolder1_Callbackconsulta_cboTipoBusca2_DDD_L"))
+    )
+    select_cnpj = comboTipoBusca.find_element_by_id("ContentPlaceHolder1_Callbackconsulta_cboTipoBusca2_DDD_L_LBI2T0")
+    select_cnpj.click()
+
+    cidade = driver.find_element_by_id('ContentPlaceHolder1_Callbackconsulta_cboCidade_I')
+    cidade.send_keys("RIO DE JANEIRO")
+
+    pesquisarBtn = driver.find_element_by_id('ContentPlaceHolder1_Callbackconsulta_btnConsultaTotal_CD')
+    pesquisarBtn.click()
+
+    WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+        EC.invisibility_of_element((By.XPATH, "//div[@class='spinner']"))
+    )
+
+    rows_per_page = WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+        EC.element_to_be_clickable((By.ID, "ContentPlaceHolder1_Callbackconsulta_dtconsultaempresa_PGT_PSI"))
+    )
+    rows_per_page.click()
+
+    combo_rows_per_page = WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+        EC.presence_of_element_located(
+            (By.ID, "ContentPlaceHolder1_Callbackconsulta_dtconsultaempresa_PGT_PSP_DXME_"))
+    )
+    select20 = combo_rows_per_page.find_element_by_xpath('//li[div[span[contains(text(), "20")]]]')
+    select20.click()
+
+    WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+        EC.invisibility_of_element((By.ID, "ContentPlaceHolder1_Callbackconsulta_dtconsultaempresa_LPV"))
+    )
+
+    WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+        EC.invisibility_of_element((By.ID, "ContentPlaceHolder1_Callbackconsulta_dtconsultaempresa_LD"))
+    )
+
+    img_next = driver.find_element_by_xpath("//img[@class='dxWeb_pNext_MetropolisBlue']")
+    rows_per_page = img_next.find_element_by_xpath('..')
+
+    while rows_per_page is not None:
+        table_results = WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+            EC.presence_of_element_located((By.ID, "ContentPlaceHolder1_Callbackconsulta_dtconsultaempresa_ICell"))
+        )
+        rows = table_results.find_elements_by_xpath(".//td[@class= 'dxdvItem_MetropolisBlue']/div")
+        print(len(rows))
+        for row in rows:
+            values = row.find_elements_by_xpath(".//div/span")
+            imobiliaria = {
+                'numero_registro': values[0].text,
+                'nome': values[1].text,
+                'situacao_cadastral': values[3].text,
+                'endereco': values[5].text,
+                'bairro': values[7].text,
+                'cidade': values[9].text,
+                'cep': values[11].text,
+                'telefone': values[13].text,
+                'responsavel_tecnico': values[15].text
+            }
+            print(imobiliaria)
+            w.writerow(imobiliaria)
+        rows_per_page.click()
+        WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+            EC.invisibility_of_element((By.ID, "ContentPlaceHolder1_Callbackconsulta_dtconsultaempresa_LPV"))
         )
 
-    def parse(self, response):
-        self.driver.get(response.url)
-
-        selectTipoPesquisa = self.driver.find_element_by_id('cbousuario_B-1')
-        selectTipoPesquisa.click()
-        comboTipoPesquisa = WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-            EC.presence_of_element_located(
-                (By.ID, "cbousuario_DDD_L_LBT"))
-        )
-        selectPJ = comboTipoPesquisa.find_element_by_xpath("//tr[td[contains(text(), 'Pessoa Jurídica')]]")
-        selectPJ.click()
-
-        WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-            EC.invisibility_of_element((By.ID, "ContentPlaceHolder1_Callbackconsulta_LPV"))
+        WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+            EC.invisibility_of_element((By.ID, "ContentPlaceHolder1_Callbackconsulta_dtconsultaempresa_LD"))
         )
 
-        selectTipoBusca = WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-            EC.element_to_be_clickable((By.ID, "ContentPlaceHolder1_Callbackconsulta_cboTipoBusca2_I"))
-        )
-        selectTipoBusca.click()
-        comboTipoBusca = WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-            EC.presence_of_element_located(
-                (By.ID, "ContentPlaceHolder1_Callbackconsulta_cboTipoBusca2_DDD_L"))
-        )
-        select_cnpj = comboTipoBusca.find_element_by_id("ContentPlaceHolder1_Callbackconsulta_cboTipoBusca2_DDD_L_LBI2T0")
-        select_cnpj.click()
-
-        cidade = self.driver.find_element_by_id('ContentPlaceHolder1_Callbackconsulta_cboCidade_I')
-        cidade.send_keys("RIO DE JANEIRO")
-
-
-        try:
-            pesquisarBtn = self.driver.find_element_by_id('ContentPlaceHolder1_Callbackconsulta_btnConsultaTotal_CD')
-            pesquisarBtn.click()
-
-            WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-                EC.invisibility_of_element((By.XPATH, "//div[@class='spinner']"))
-            )
-
-            rows_per_page = WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-                EC.element_to_be_clickable((By.ID, "ContentPlaceHolder1_Callbackconsulta_dtconsultaempresa_PGT_PSI"))
-            )
-            rows_per_page.click()
-
-            combo_rows_per_page = WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-                EC.presence_of_element_located(
-                    (By.ID, "ContentPlaceHolder1_Callbackconsulta_dtconsultaempresa_PGT_PSP_DXME_"))
-            )
-            select20 = combo_rows_per_page.find_element_by_xpath('//li[div[span[contains(text(), "20")]]]')
-            select20.click()
-
-            WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-                EC.invisibility_of_element((By.ID, "ContentPlaceHolder1_Callbackconsulta_dtconsultaempresa_LPV"))
-            )
-
-            WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-                EC.invisibility_of_element((By.ID, "ContentPlaceHolder1_Callbackconsulta_dtconsultaempresa_LD"))
-            )
-
-            img_next = self.driver.find_element_by_xpath("//img[@class='dxWeb_pNext_MetropolisBlue']")
-            rows_per_page = img_next.find_element_by_xpath('..')
-
-            while rows_per_page is not None:
-                table_results = self.driver.find_element_by_id("ContentPlaceHolder1_Callbackconsulta_dtconsultaempresa_ICell")
-                rows = table_results.find_elements_by_xpath("//tr/td[@class= 'dxdvItem_MetropolisBlue']/div")
-                for row in rows:
-                    values = row.find_elements_by_xpath("//div/span")
-                    yield {
-                        'numero_registro': values[6].text,
-                        'nome': values[7].text,
-                        'situacao_cadastral': values[9].text,
-                        'endereco': values[11].text,
-                        'bairro': values[13].text,
-                        'cidade': values[15].text,
-                        'cep': values[17].text,
-                        'telefone': values[19].text,
-                        'responsavel_tecnico': values[21].text
-                    }
-                rows_per_page.click()
-                WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-                    EC.invisibility_of_element((By.ID, "ContentPlaceHolder1_Callbackconsulta_dtconsultaempresa_LPV"))
-                )
-
-                WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-                    EC.invisibility_of_element((By.ID, "ContentPlaceHolder1_Callbackconsulta_dtconsultaempresa_LD"))
-                )
-
-                img_next = self.driver.find_element_by_xpath("//img[@class='dxWeb_pNext_MetropolisBlue']")
-                rows_per_page = img_next.find_element_by_xpath('..')
-
-        finally:
-            self.driver.quit()
+        img_next = driver.find_element_by_xpath("//img[@class='dxWeb_pNext_MetropolisBlue']")
+        rows_per_page = img_next.find_element_by_xpath('..')
+    
+driver.close()
--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 #pylint: disable=too-many-function-args
-import scrapy
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -9,88 +8,86 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 DEFAULT_TIMEOUT = 120
 
-class CreciSpider(scrapy.Spider):
-    name = 'creciSpider'
-    start_urls = ['https://servico.creci-rj.gov.br/spw/ConsultaCadastral/TelaConsultaPubCompleta.aspx']
+name = 'creciSpider'
+start_urls = ['https://servico.creci-rj.gov.br/spw/ConsultaCadastral/TelaConsultaPubCompleta.aspx']
 
-    def __init__(self):
-        profile = webdriver.FirefoxProfile()
-        profile.accept_untrusted_certs = True
-        profile.assume_untrusted_cert_issuer = False
-        profile.set_preference('security.tls.version.max', 2)
-        profile.set_preference('security.tls.version.min', 1)
-        cap = DesiredCapabilities().FIREFOX
-        cap["marionete"] = False
-        opts = Options()
-        opts.headless = True
-        self.driver = webdriver.Firefox(
-            firefox_profile=profile,
-            capabilities=cap,
-            options=opts
+profile = webdriver.FirefoxProfile()
+profile.accept_untrusted_certs = True
+profile.assume_untrusted_cert_issuer = False
+profile.set_preference('security.tls.version.max', 2)
+profile.set_preference('security.tls.version.min', 1)
+cap = DesiredCapabilities().FIREFOX
+cap["marionete"] = False
+opts = Options()
+opts.headless = True
+driver = webdriver.Firefox(
+    firefox_profile=profile,
+    capabilities=cap,
+    options=opts
+)
+
+driver.get(start_urls[0])
+
+tipoPesquisa = driver.find_element_by_id('cbousuario_I')
+tipoPesquisa.send_keys("Profissional")
+
+tipoBusca = driver.find_element_by_id('ContentPlaceHolder1_Callbackconsulta_cboTipoBusca_I')
+tipoBusca.send_keys("Num. Registro")
+
+cidade = driver.find_element_by_id('ContentPlaceHolder1_Callbackconsulta_cboCidade_I')
+cidade.send_keys("RIO DE JANEIRO")
+
+try:
+    selectedCidade = WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+        EC.element_to_be_clickable((By.ID, 'ContentPlaceHolder1_Callbackconsulta_cboCidade_DDD_L_LBI370T0'))
+    )
+    selectedCidade.click()
+
+    WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+        EC.invisibility_of_element((By.XPATH, "//div[@class='spinner']"))
+    )
+
+    pesquisarBtn = driver.find_element_by_id('ContentPlaceHolder1_Callbackconsulta_btnConsultaTotal')
+    pesquisarBtn.click()
+
+    WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+        EC.invisibility_of_element((By.XPATH, "//div[@class='spinner']"))
+    )
+
+    rowsPerPage = driver.find_element_by_id("ContentPlaceHolder1_Callbackconsulta_gridConsulta_DXPagerBottom_DDB")
+    rowsPerPage.click()
+
+    comboRowsPerPage = WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+        EC.presence_of_element_located((By.ID, "ContentPlaceHolder1_Callbackconsulta_gridConsulta_DXPagerBottom_PSP_DXME_"))
+    )
+    select200 = comboRowsPerPage.find_element_by_xpath('//li[div[span[contains(text(), "200")]]]')
+    select200.click()
+
+    WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+        EC.invisibility_of_element((By.XPATH, "//div[@class='spinner']"))
+    )
+
+    imgNext = driver.find_element_by_xpath("//img[@class='dxWeb_pNext_MetropolisBlue']")
+    rowsPerPage = imgNext.find_element_by_xpath('..')
+
+    while rowsPerPage is not None:
+        rows = driver.find_elements_by_class_name("dxgvDataRow_MetropolisBlue")
+        for row in rows:
+            values = row.find_elements_by_class_name("letrasistema")
+            imobiliaria = {
+                'no_registro': values[0].text,
+                'nome': values[1].text,
+                'categoria': values[2].text,
+                'categoria2': values[3].text,
+                'situacao': values[4].text
+            }
+            print(imobiliaria)
+        rowsPerPage.click()
+        WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+            EC.invisibility_of_element((By.XPATH, "//div[@class='spinner']"))
         )
+        imgNext = driver.find_element_by_xpath("//img[@class='dxWeb_pNext_MetropolisBlue']")
+        rowsPerPage = imgNext.find_element_by_xpath('..')
 
-    def parse(self, response):
-        self.driver.get(response.url)
-
-        tipoPesquisa = self.driver.find_element_by_id('cbousuario_I')
-        tipoPesquisa.send_keys("Profissional")
-
-        tipoBusca = self.driver.find_element_by_id('ContentPlaceHolder1_Callbackconsulta_cboTipoBusca_I')
-        tipoBusca.send_keys("Num. Registro")
-
-        cidade = self.driver.find_element_by_id('ContentPlaceHolder1_Callbackconsulta_cboCidade_I')
-        cidade.send_keys("RIO DE JANEIRO")
-
-        try:
-            selectedCidade = WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-                EC.element_to_be_clickable((By.ID, 'ContentPlaceHolder1_Callbackconsulta_cboCidade_DDD_L_LBI370T0'))
-            )
-            selectedCidade.click()
-
-            WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-                EC.invisibility_of_element((By.XPATH, "//div[@class='spinner']"))
-            )
-
-            pesquisarBtn = self.driver.find_element_by_id('ContentPlaceHolder1_Callbackconsulta_btnConsultaTotal')
-            pesquisarBtn.click()
-
-            WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-                EC.invisibility_of_element((By.XPATH, "//div[@class='spinner']"))
-            )
-
-            rowsPerPage = self.driver.find_element_by_id("ContentPlaceHolder1_Callbackconsulta_gridConsulta_DXPagerBottom_DDB")
-            rowsPerPage.click()
-
-            comboRowsPerPage = WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-                EC.presence_of_element_located((By.ID, "ContentPlaceHolder1_Callbackconsulta_gridConsulta_DXPagerBottom_PSP_DXME_"))
-            )
-            select200 = comboRowsPerPage.find_element_by_xpath('//li[div[span[contains(text(), "200")]]]')
-            select200.click()
-
-            WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-                EC.invisibility_of_element((By.XPATH, "//div[@class='spinner']"))
-            )
-
-            imgNext = self.driver.find_element_by_xpath("//img[@class='dxWeb_pNext_MetropolisBlue']")
-            rowsPerPage = imgNext.find_element_by_xpath('..')
-
-            while rowsPerPage is not None:
-                rows = self.driver.find_elements_by_class_name("dxgvDataRow_MetropolisBlue")
-                for row in rows:
-                    values = row.find_elements_by_class_name("letrasistema")
-                    yield {
-                        'no_registro': values[0].text,
-                        'nome': values[1].text,
-                        'categoria': values[2].text,
-                        'categoria2': values[3].text,
-                        'situacao': values[4].text
-                    }
-                rowsPerPage.click()
-                WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(
-                    EC.invisibility_of_element((By.XPATH, "//div[@class='spinner']"))
-                )
-                imgNext = self.driver.find_element_by_xpath("//img[@class='dxWeb_pNext_MetropolisBlue']")
-                rowsPerPage = imgNext.find_element_by_xpath('..')
-
-        finally:
-            self.driver.quit()
+finally:
+    driver.quit()
